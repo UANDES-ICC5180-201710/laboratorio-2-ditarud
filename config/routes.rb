f0fca45ce1f54ff8df5e75e015a8ba8a64be086f
ditarud <ditarud@miuandes.cl>
2017-03-26 21:41:18 -0300

    Lab02

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :person_courses
+  resources :courses
+  resources :roles
+  resources :people
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
